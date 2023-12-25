da6af04d73c6b6a40fc3ac0b97e5aa76b5308815
AYUSH KUMAR SINGH <ayushkumarsingh0708@gmail.com>
2023-12-25 16:28:41 +0530

    Minor changes

--- lib/bookmysleep ui/home_page.dart
+++ lib/bookmysleep ui/home_page.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ui/bookmysleep%20ui/house_box.dart';
+import 'package:ui/bookmysleep%20ui/widgets/house_box.dart';
 
 import 'package:ui/constants.dart';
 
